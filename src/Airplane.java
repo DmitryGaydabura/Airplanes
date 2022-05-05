5cfc00c8e7456a39d58bde8672382b61dbba9cad
DmitryGaydabura <gaydabura.d@icloud.com>
2022-05-05 22:09:49 +0300

    Initial commit

---
+++ src/Airplane.java
@@ -0,0 +1,20 @@
+public class Airplane {
+    String airplaneName;
+    String modelOfAirplane;
+    int numberOfStandardPassengers;
+    int numberOfBusinessClassPassengers;
+
+    public static void printAirplaneInfo(String airplaneName, String modelOfAirplane) {
+        System.out.println("Type of Airplane : \nName: " + airplaneName + " ,\nModel: " + modelOfAirplane + ".");
+    }
+
+    public static void printAirplaneInfo(String airplaneName, String modelOfAirplane, int numberOfStandardPassengers) {
+        System.out.println("Type of Airplane : \nName: " + airplaneName + " ,\nModel: " + modelOfAirplane + " ,\nStandart Seats: " + numberOfStandardPassengers + ".");
+    }
+
+    public static void printAirplaneInfo(String airplaneName, String modelOfAirplane, int numberOfStandardPassengers, int numberOfBusinessClassPassengers) {
+        System.out.println("Type of Airplane : \nName: " + airplaneName + " ,\nModel: " + modelOfAirplane + " ,\nStandart Seats: " + numberOfStandardPassengers + " ,\nNumber of Business-Class Seats: " + numberOfBusinessClassPassengers + ".");
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        Airplane.printAirplaneInfo("Airliner", "Boeing 777",180,28);
+    }
+}
